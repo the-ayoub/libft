38dfed36af88c65711963e64ac6c716f83debe36
Ayoub <aybelhaj@car8s5.42barcelona.com>
2024-09-30 21:34:58 +0200

    me

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aybelhaj <aybelhaj@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 03:54:07 by aybelhaj          #+#    #+#             */
-/*   Updated: 2024/09/30 21:26:12 by aybelhaj         ###   ########.fr       */
+/*   Updated: 2024/09/30 21:34:49 by aybelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strchr(const char *s, int c)
 
 	i = 0;
 	if (c == 0)
-		return ((char *)s);
+		return (NULL);
 	while (s[i])
 	{
 		if (s[i] == (unsigned char)c)
