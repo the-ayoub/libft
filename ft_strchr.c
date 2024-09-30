716ac8fc490750a26d3d1782601c42c334d2a759
Ayoub <aybelhaj@car8s5.42barcelona.com>
2024-09-30 22:01:06 +0200

    ft_strchr.c fixed

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aybelhaj <aybelhaj@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 03:54:07 by aybelhaj          #+#    #+#             */
-/*   Updated: 2024/09/30 21:34:49 by aybelhaj         ###   ########.fr       */
+/*   Updated: 2024/09/30 22:00:16 by aybelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	if (c == 0)
-		return (NULL);
 	while (s[i])
 	{
 		if (s[i] == (unsigned char)c)
 			return ((char *)&s[i]);
 		i++;
 	}
+	if ((unsigned char)c == '\0')
+                return ((char *)&s[i]);
 	return (NULL);
 }
